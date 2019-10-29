2400f68c31f0d65fe6a951efb182cfc5f399f6ba
briskneez <54325502+briskneez@users.noreply.github.com>
2019-10-29 11:39:45 -0500

    h

--- p5/Week11gyrocollect/sketch.js
+++ p5/Week11gyrocollect/sketch.js
@@ -19,11 +19,11 @@ function setup() {
   gamma = 0;
 
   bunnyImage = loadImage("assets/bunny.jpg");
- imageMode(CENTER);
- rectMode(CENTER);
+  imageMode(CENTER);
+  rectMode(CENTER);
 
   rectMode(CENTER);
-  for (var i = 0; i < 40; i++){
+  for (var i = 0; i < 40; i++) {
     cars.push(new Car());
   }
 
@@ -51,99 +51,99 @@ function draw() {
 
   pop();
 
-  for (var i = 0; i < cars.length; i++){
-  cars[i].display();
-  cars[i].drive();
+  for (var i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].drive();
 
-  //collision detection
-  if(cars[i].pos.dist(frogPos) < 20){
-    cars.splice(i, 1);
+    //collision detection
+    if (cars[i].pos.dist(frogPos) < 20) {
+      cars.splice(i, 1);
+    }
   }
-}
-//frog
+  //frog
   //fill('green');
   //ellipse(frogPos.x, frogPos.y, 50, 50);
-  image(bunnyImage, frogPos.x, frogPos.y, 50, 50);
+  image(bunnyImage, frogPos.x, frogPos.y, 100, 100);
   checkForKeys(); //this moves the frog
 
   // DECORATIONS
 
- // Just a bunch of text commands to display data coming in from addEventListeners
- textAlign(LEFT);
- textSize(20);
- fill('black');
- text("orientation data:", 25, 25);
- textSize(15);
- text("alpha: " + alpha, 25, 50);
- text("beta: " + beta, 25, 70);
- text("gamma: " + gamma, 25, 90);
- textSize(20);
- text("acceleration data:", 25, 125);
- textSize(15);
- text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
- text("y = " + y.toFixed(2), 25, 170);
- text("z = " + z.toFixed(4), 25, 190);
- frogPos.x= xPosition;
- frogPos.y= yPosition;
+  // Just a bunch of text commands to display data coming in from addEventListeners
+  textAlign(LEFT);
+  textSize(20);
+  fill('black');
+  text("orientation data:", 25, 25);
+  textSize(15);
+  text("alpha: " + alpha, 25, 50);
+  text("beta: " + beta, 25, 70);
+  text("gamma: " + gamma, 25, 90);
+  textSize(20);
+  text("acceleration data:", 25, 125);
+  textSize(15);
+  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  text("y = " + y.toFixed(2), 25, 170);
+  text("z = " + z.toFixed(4), 25, 190);
+  frogPos.x = xPosition;
+  frogPos.y = yPosition;
 
- // HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
-
-
-
-// Read in accelerometer data
-
-window.addEventListener('deviceorientation', function(e) {
-
-  alpha = e.alpha;
-
-  beta = e.beta;
-
-  gamma = e.gamma;
-
-});
+  // HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
 
 
 
+  // Read in accelerometer data
+
+  window.addEventListener('deviceorientation', function(e) {
+
+    alpha = e.alpha;
+
+    beta = e.beta;
+
+    gamma = e.gamma;
+
+  });
 
 
-// accelerometer Data
 
-window.addEventListener('devicemotion', function(e) {
 
-  // get accelerometer values
 
-  x = e.acceleration.x;
+  // accelerometer Data
 
-  y = e.acceleration.y;
+  window.addEventListener('devicemotion', function(e) {
 
-  z = e.acceleration.z;
+    // get accelerometer values
 
-});
+    x = e.acceleration.x;
+
+    y = e.acceleration.y;
+
+    z = e.acceleration.z;
+
+  });
 }
 //our car class
-function Car(){
+function Car() {
   this.pos = createVector(random(width), random(height));
   this.vel = createVector(random(-5, 5), random(-5, 5));
   this.r = random(255);
   this.g = random(255);
   this.b = random(255);
 
-  this.display = function(){
+  this.display = function() {
     fill(this.r, this.g, this.b);
     rect(this.pos.x, this.pos.y, 100, 50);
   }
 
-  this.drive = function(){
+  this.drive = function() {
     this.pos.add(this.vel);
-    if(this.pos.x > width) this.pos.x = 0;
-    if(this.pos.x < 0) this.pos.x = width;
-    if(this.pos.y > height) this.pos.y = 0;
-    if(this.pos.y < 0) this.pos.y = height;
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
   }
 
 }
 
-function checkForKeys(){
+function checkForKeys() {
   if (keyIsDown(LEFT_ARROW)) frogPos.x = frogPos.x - 5;
   if (keyIsDown(RIGHT_ARROW)) frogPos.x = frogPos.x + 5;
   if (keyIsDown(UP_ARROW)) frogPos.y = frogPos.y - 5;
