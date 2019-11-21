f26eb3c0576b55427c094d92bc3cb37e4ec98480
briskneez <54325502+briskneez@users.noreply.github.com>
2019-11-21 11:15:02 -0600

    hola

---
+++ p5/Stream-Two-Project-master/index.js
@@ -0,0 +1,229 @@
+let commands = ["Bop It", "Twist It", "Pull It"];
+let timerDisplay = document.querySelector("#timer");
+let score = 0;
+let localStorageName = "bopItScore";
+let highScore;
+
+// Chooses a random command from commands array
+function chooseRandomCommand(a) {
+    return commands[Math.floor(Math.random() * commands.length)];
+}
+
+
+function showCommand(command) {
+    if (command === "Bop It") {
+        $("h3").removeClass("bops");
+    }
+    if (command === "Twist It") {
+        $("h3").removeClass("twists");
+    }
+    if (command === "Pull It") {
+        $("h3").removeClass("pulls");
+    }
+}
+
+// Starts game by clearing anything left over from a previous
+// game before calling playGame
+function startGame() {
+    endGame.called = false;
+    resetScore();
+    score = 0;
+    startTimerControls(30, timerDisplay);
+    playGame();
+}
+
+
+function playGame() {
+    // Disables start button to prevent timer restarting
+    document.getElementById("startButton").disabled = true;
+
+    // Enables game buttons
+    document.getElementById("bopIt").disabled = false;
+    document.getElementById("twistIt").disabled = false;
+    document.getElementById("pullIt").disabled = false;
+    document.getElementById("bops").classList.add("bops");
+    document.getElementById("pulls").classList.add("pulls");
+    document.getElementById("twists").classList.add("twists");
+    document.getElementById("losing").classList.add("losing");
+    let command = chooseRandomCommand();
+    let bopItButton =
+        document.getElementById("bopIt").getAttribute("data-type");
+    let pullItButton =
+        document.getElementById("pullIt").getAttribute("data-type");
+    let twistItButton =
+        document.getElementById("twistIt").getAttribute("data-type");
+    showCommand(command);
+
+
+    document.getElementById("bopIt").onclick = function() {
+        if (command === bopItButton) {
+            updateScoreBoard();
+            document.getElementById("bops").classList.add("bops");
+            playGame();
+        }
+        else {
+            endGame();
+        }
+    };
+
+
+    document.getElementById("pullIt").onclick = function() {
+        if (command === pullItButton) {
+            updateScoreBoard();
+            document.getElementById("pulls").classList.add("pulls");
+            playGame();
+        }
+        else {
+            endGame();
+        }
+    };
+
+
+    document.getElementById("twistIt").onclick = function() {
+        if (command === twistItButton) {
+            updateScoreBoard();
+            document.getElementById("twists").classList.add("twists");
+            playGame();
+        }
+        else {
+            endGame();
+
+        }
+    };
+
+
+    // Hotkey functionality
+    document.onkeyup = function(a) {
+        // "A" key for "Twist It"
+        if (a.which === 65) {
+            document.getElementById("twistIt").getAttribute("data-type");
+            if (command === twistItButton && a.which === 65) {
+                updateScoreBoard();
+                document.getElementById("twists").classList.add("twists");
+                playGame();
+            }
+            else {
+                endGame();
+            }
+        }
+        // "P" Key for "Pull It"
+        else if (a.which === 80) {
+            document.getElementById("pullIt").getAttribute("data-type");
+            if (command === pullItButton && a.which === 80) {
+                updateScoreBoard();
+                document.getElementById("pulls").classList.add("pulls");
+                playGame();
+            }
+            else {
+                endGame();
+            }
+        }
+        // "B" Key for "Bop It"
+        else if (a.which === 66) {
+            document.getElementById("bopIt").getAttribute("data-type");
+            if (command === bopItButton && a.which === 66) {
+                updateScoreBoard();
+                document.getElementById("bops").classList.add("bops");
+                playGame();
+            }
+            else {
+                endGame();
+            }
+        }
+    };
+}
+
+
+function updateScoreBoard() {
+    score++;
+    let scoreBox = document.getElementById("score");
+    scoreBox.textContent = score;
+}
+
+
+function resetScore() {
+    let score = 0;
+    let scoreBox = document.getElementById("score");
+    scoreBox.textContent = score;
+}
+
+
+function updateHighScoreBoard() {
+    if (localStorage.getItem(localStorageName) === null) {
+        highScore = 0;
+    }
+    else {
+        highScore = localStorage.getItem(localStorageName);
+    }
+    highScore = Math.max(score, highScore);
+    localStorage.setItem(localStorageName, highScore);
+    let highScoreBox = document.getElementById("highScore");
+    highScoreBox.textContent = highScore;
+}
+
+
+function startTimerControls(duration, timerDisplay) {
+    let timer = duration,
+        minutes, seconds;
+    let timesRan = 0;
+    let interval = setInterval(function() {
+        timesRan += 1;
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        timerDisplay.textContent = minutes + ":" + seconds;
+
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+
+        // if timer runs out, calls endGame to end the game
+        if (timesRan === 31) {
+            endGame();
+            clearInterval(interval);
+        }
+
+        // if endGame is called (i.e. incorrect answer), clears timer
+        if (endGame.called === true) {
+            clearInterval(interval);
+        }
+
+    }, 1000);
+}
+
+
+function endGame() {
+    // Signals to startTimer to stop the timer
+    endGame.called = true;
+    document.getElementById("bops").classList.add("bops");
+    document.getElementById("pulls").classList.add("pulls");
+    document.getElementById("twists").classList.add("twists");
+    $("h3").removeClass("losing");
+
+    // Disables game buttons to prevent cheating
+    document.getElementById("bopIt").disabled = true;
+    document.getElementById("twistIt").disabled = true;
+    document.getElementById("pullIt").disabled = true;
+
+    // Enables start button to restart game
+    document.getElementById("startButton").disabled = false;
+
+    // Disables hotkeys to prevent cheating
+    document.onkeyup = function(a) {
+        (a.which !== 80 && a.which !== 65 && a.which !== 66);
+    };
+    updateHighScoreBoard();
+}
+
+
+// Modal on page load
+function loadModal() {
+    $('#rotateScreenModal').modal('show');
+
+    // Clears high score
+    window.onload = window.localStorage.clear();
+}
--- p5/mp4/sketch.js
+++ p5/mp4/sketch.js
@@ -1,10 +1,277 @@
-function setup() {
-  // put setup code here
-  createCanvas(800, 800);
+var myState = 0;
+
+var myTimer = 0;
+
+let value = 0;
+
+let mic;
+
+var song;
+var song1;
+var song2;
+var song3;
+
+function preload() {
+  song = loadSound('assets/rec1.m4a');
+  song1 = loadSound('assets/rec2.m4a');
+  song2 = loadSound('assets/rec3.m4a');
+  song3 = loadSound('assets/rec4.m4a');
+
+  song.loop();
+  song.stop();
+  song1.loop();
+  song1.stop();
+  song2.loop();
+  song2.stop();
+  song3.loop();
+  song3.stop();
+
 
 }
 
+
+
+function setup() {
+
+  // put setup code here
+
+  createCanvas(windowWidth, windowHeight);
+
+  mic = new p5.AudioIn();
+
+  mic.start();
+
+}
+
+
+
 function draw() {
+
+  let vol = mic.getLevel();
+
   // put drawing code here
-  
+
+  fill(value);
+
+  rect(25, 25, 50, 50);
+
+
+
+  switch (myState) {
+
+    case 0:
+
+      background('red');
+
+      textSize(72);
+
+      text("HOME", width / 2, height / 2);
+
+      myTimer++;
+
+      if (mouseIsPressed) {
+
+        myTimer = 0;
+
+        myState = 100;
+
+      }
+
+      break;
+
+
+
+    case 1:
+
+      song.play();
+      myState = 11;
+
+      break;
+
+    case 2:
+
+      song1.play();
+      myState = 12;
+      break;
+
+
+
+    case 3:
+
+    song2.play();
+    myState = 13;
+
+      break;
+
+
+
+    case 4:
+    song3.play();
+    myState = 14;
+
+    break;
+
+    case 5:
+
+      background('purple');
+
+      textSize(72);
+
+      text("LOSE", width / 2, height / 2);
+
+      myTimer++;
+
+      if (mouseIsPressed) {
+
+        myTimer = 0;
+
+        myState = 0;
+
+      }
+
+      break;
+
+      case 11:
+      background('green');
+
+      textSize(72);
+
+      text("BOP IT", width / 2, height / 2);
+
+      myTimer++;
+
+      if (myTimer >= 500) {
+
+        myState = 5;
+
+      }
+
+
+
+      if (mouseIsPressed) {
+
+        myTimer = 0;
+
+        myState = 100;
+
+      }
+      break;
+
+      case 12:
+      background('blue');
+
+      textSize(72);
+
+      text("SHAKE IT", width / 2, height / 2);
+
+      myTimer++;
+
+      if (myTimer >= 500) {
+
+        myState = 5;
+
+        myTimer = 0;
+
+      }
+      break;
+
+      case 13:
+      background('yellow');
+
+      textSize(72);
+
+      text("YELL IT", width / 2, height / 2);
+
+      myTimer++;
+
+      if (myTimer >= 500) {
+
+        myTimer = 0;
+
+        myState = 5;
+
+      }
+
+      console.log(vol);
+
+      if (vol > 0.12) {
+
+        myTimer = 0;
+
+        myState = 100;
+
+      }
+
+      break;
+
+      case 14:
+
+            background('orange');
+
+            textSize(72);
+            text("PASS IT", width / 2, height / 2);
+
+            myTimer++;
+
+            if (myTimer >= 500) {
+
+              myTimer = 0;
+
+              myState = 100;
+
+            }
+
+            break;
+
+
+
+
+    case 100:
+
+      song.pause();
+      song1.pause();
+      song2.pause();
+      song3.pause();
+      myState = floor(random(1, 5));
+
+      break;
+
+  }
+
+}
+
+
+
+function deviceShaken() {
+
+  if (myState == 2) {
+
+    myState = 100;
+
+    myTimer = 0;
+
+  }
+
+}
+
+
+
+/*   function mouseReleased() {
+
+    myState = myState + 1;
+
+    if (myState > 6) {
+
+    myState = 0;
+
+    }
+
+  }
+
+   */
+
+function touchStarted() {
+
+  getAudioContext().resume();
+
 }
